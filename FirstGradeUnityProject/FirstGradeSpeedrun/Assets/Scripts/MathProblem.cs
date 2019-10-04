1c8677b51f544078e0d294d4f794bc2b9d080c70
Eddie Brazier <54645487+EddieBrazier@users.noreply.github.com>
2019-10-03 22:51:09 -0400

    Created Project
    made the unity project and created a basic math problem prefab

---
+++ FirstGradeUnityProject/FirstGradeSpeedrun/Assets/Scripts/MathProblem.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*Eddie Brazier
+ *Personal Project: Are you faster than a first grader?
+ *10/3/2019
+ */
+
+public class MathProblem : MonoBehaviour
+{
+    //field for the text component of the current gameObject
+    private Text problemText;
+
+    //field for problem solution
+    public int problemSolution;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get the text component of the game object
+        problemText = gameObject.GetComponent<Text>();
+
+        //roll a random number to determine if the problem will be addition or subtraction
+        int problemInt = Random.Range(0, 2);
+
+        if (problemInt == 0)
+        {
+            problemText.text = AdditionProblem();
+        }
+        else
+        {
+            problemText.text = AdditionProblem();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// method that creates  random addition problem to solve 
+    /// </summary>
+    /// <returns></returns>
+    private string AdditionProblem()
+    {
+        //random numbers to generate addition string
+        int numOne = Random.Range(0, 10);
+        int numTwo = Random.Range(0, 10);
+
+        //create string for the math problem
+        string addProblem = numOne + " + " + numTwo + " = ";
+
+        //get the solution to the problem
+        AdditionSolution(numOne, numTwo);
+
+        return addProblem;
+    }
+
+    private void AdditionSolution(int numOne, int NumTwo)
+    {
+        //set problem solution to the sum of passed-in numbers
+        problemSolution = numOne + NumTwo;
+    }
+}
---
+++ FirstGradeUnityProject/FirstGradeSpeedrun/Assets/Scripts/TextInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*Eddie Brazier
+ *Personal Project: Are you faster than a first grader?
+ *10/3/2019
+ */
+
+public class TextInput : MonoBehaviour
+{
+    //field for the textinput ui component of the GameObject
+    private InputField mathInput;
+
+    //field for the string being held in the input field and the int to parse it to
+    private string inputString;
+    private int answerInt;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get text input component
+        mathInput = gameObject.GetComponent<InputField>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
